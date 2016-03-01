b7011469993772ca1518dba3cd7351105c75f0d0
Lucas deGomez <lucasdegomez@gmail.com>
2016-03-01 17:13:27 +0100

    basic navigation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,23 @@
-project_list = [
-  ["Redradix Posters", "Technology posters created by redradix.", "", []],
-  ["Asset-O-Tron", "Final project of Talentum Empleo developed by Ronald Romero, Nerea Díaz, Diego Sánchez, Lucas Gómez y Marco Antonio López de la Nieta.", "", [1,2,3,4,5,6]]
-]
-
-project_list.each do |name,description,url,resource_ids|
-  Project.create(name: name, description: description, url: url, resource_ids: resource_ids)
-end
-
-resource_list =[
-  ["Ruby", "Ruby poster created by redradix", "https://github.com/redradix/posters/raw/master/png/ruby.png", [1, 2]],
-  ["AngularJS", "AngularJS poster created by redradix", "https://github.com/redradix/posters/raw/master/png/angular.png", [1, 2]],
-  ["CSS3", "CSS3 poster created by redradix", "https://github.com/redradix/posters/raw/master/png/css3.png", [1, 2]],
-  ["HTML5", "HTML5 poster created by redradix", "https://raw.githubusercontent.com/redradix/posters/master/png/html5.png", [1, 2]],
-  ["Javascript", "Javascript poster created by redradix", "https://raw.githubusercontent.com/redradix/posters/master/png/javascript.png", [1, 2]],
-  ["Node.js", "Node.js poster created by redradix", "https://github.com/redradix/posters/raw/master/png/node.png", [1, 2]],
-  ["Less.js", "Less.js poster created by redradix", "https://github.com/redradix/posters/raw/master/png/less.png", [1]],
-  ["Sass", "Sass poster created by redradix", "https://github.com/redradix/posters/raw/master/png/sass.png", [1]],
-]
-
-resource_list.each do |name,description,url,project_ids|
-  Resource.create(name: name, description: description, url: url, project_ids: project_ids)
-end
+# project_list = [
+#   ["Redradix Posters", "Technology posters created by redradix.", "", []],
+#   ["Asset-O-Tron", "Final project of Talentum Empleo developed by Ronald Romero, Nerea Díaz, Diego Sánchez, Lucas Gómez y Marco Antonio López de la Nieta.", "", [1,2,3,4,5,6]]
+# ]
+#
+# project_list.each do |name,description,url,resource_ids|
+#   Project.create(name: name, description: description, url: url, resource_ids: resource_ids)
+# end
+#
+# resource_list =[
+#   ["Ruby", "Ruby poster created by redradix", "https://github.com/redradix/posters/raw/master/png/ruby.png", [1, 2]],
+#   ["AngularJS", "AngularJS poster created by redradix", "https://github.com/redradix/posters/raw/master/png/angular.png", [1, 2]],
+#   ["CSS3", "CSS3 poster created by redradix", "https://github.com/redradix/posters/raw/master/png/css3.png", [1, 2]],
+#   ["HTML5", "HTML5 poster created by redradix", "https://raw.githubusercontent.com/redradix/posters/master/png/html5.png", [1, 2]],
+#   ["Javascript", "Javascript poster created by redradix", "https://raw.githubusercontent.com/redradix/posters/master/png/javascript.png", [1, 2]],
+#   ["Node.js", "Node.js poster created by redradix", "https://github.com/redradix/posters/raw/master/png/node.png", [1, 2]],
+#   ["Less.js", "Less.js poster created by redradix", "https://github.com/redradix/posters/raw/master/png/less.png", [1]],
+#   ["Sass", "Sass poster created by redradix", "https://github.com/redradix/posters/raw/master/png/sass.png", [1]],
+# ]
+#
+# resource_list.each do |name,description,url,project_ids|
+#   Resource.create(name: name, description: description, url: url, project_ids: project_ids)
+# end
